e1af9f9e444f7b457b8b873255e7d74aa869520c
Mike Krüger <mkrueger@xamarin.com>
2013-06-14 22:06:59 +0200

    Added ImplementNotImplementedProperty.

--- ICSharpCode.NRefactory.CSharp/Refactoring/ImplementNotImplementedProperty.cs
+++ ICSharpCode.NRefactory.CSharp/Refactoring/ImplementNotImplementedProperty.cs
@@ -1,21 +1,21 @@
-﻿// 
-// CreateBackingStore.cs
+//
+// ImplementNotImplementedProperty.cs
 //
 // Author:
-//       Mike Krüger <mkrueger@novell.com>
-// 
-// Copyright (c) 2011 Mike Krüger <mkrueger@novell.com>
-// 
+//       Mike Krüger <mkrueger@xamarin.com>
+//
+// Copyright (c) 2013 Xamarin Inc. (http://xamarin.com)
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,31 +23,49 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ICSharpCode.NRefactory.CSharp.Refactoring
 {
-	[ContextAction("Create backing store for auto property", Description = "Creates a backing field for an auto property.")]
-	public class CreateBackingStoreAction : ICodeActionProvider
+	[ContextAction("Create a backing field for a not implemented property", Description = "Creates a backing field for a not implemented property.")]
+	public class ImplementNotImplementedProperty : ICodeActionProvider
 	{
+		bool IsNotImplemented(RefactoringContext context, BlockStatement body)
+		{
+			if (body.IsNull)
+				return true;
+			if (body.Statements.Count == 1) {
+				var throwStmt = body.Statements.First () as ThrowStatement;
+				if (throwStmt != null) {
+					return context.Resolve (throwStmt.Expression).Type.FullName == "System.NotImplementedException";
+				}
+			}
+			return false;
+		}
+
 		public IEnumerable<CodeAction> GetActions(RefactoringContext context)
 		{
-			var property = context.GetNode<PropertyDeclaration>();
-			if (!(property != null &&
-			      !property.Getter.IsNull && !property.Setter.IsNull && // automatic properties always need getter & setter
-			      property.Getter.Body.IsNull &&
-			      property.Setter.Body.IsNull)) {
+			var property = context.GetNode<PropertyDeclaration> ();
+			if (property == null ||
+			    !IsNotImplemented (context, property.Getter.Body) ||
+			    !IsNotImplemented (context, property.Setter.Body)) {
 				yield break;
 			}
 			
-			yield return new CodeAction(context.TranslateString("Create backing store"), script => {
+			yield return new CodeAction(context.TranslateString("Implement property"), script => {
 				string backingStoreName = context.GetNameProposal (property.Name);
 				
 				// create field
 				var backingStore = new FieldDeclaration ();
 				if (property.Modifiers.HasFlag (Modifiers.Static))
 					backingStore.Modifiers |= Modifiers.Static;
+
+				if (property.Setter.IsNull)
+					backingStore.Modifiers |= Modifiers.Readonly;
+				
 				backingStore.ReturnType = property.ReturnType.Clone ();
 				
 				var initializer = new VariableInitializer (backingStoreName);
@@ -64,13 +82,18 @@ namespace ICSharpCode.NRefactory.CSharp.Refactoring
 				newProperty.Getter.Body = new BlockStatement () {
 					new ReturnStatement (id1)
 				};
-				newProperty.Setter.Body = new BlockStatement () {
-					new AssignmentExpression (id2, AssignmentOperatorType.Assign, new IdentifierExpression ("value"))
-				};
+				if (!property.Setter.IsNull) {
+					newProperty.Setter.Body = new BlockStatement () {
+						new AssignmentExpression (id2, AssignmentOperatorType.Assign, new IdentifierExpression ("value"))
+					};
+				}
 				
 				script.Replace (property, newProperty);
 				script.InsertBefore (property, backingStore);
-				script.Link (initializer, id1, id2);
+				if (!property.Setter.IsNull)
+					script.Link (initializer, id1, id2);
+				else
+					script.Link (initializer, id1);
 			}, property.NameToken);
 		}
 	}
