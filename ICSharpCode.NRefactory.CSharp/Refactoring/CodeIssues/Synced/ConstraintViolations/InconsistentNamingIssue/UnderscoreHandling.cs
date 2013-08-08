cbde65a12ffb899f0759d968563911a5dd3fc517
Mike Krüger <mkrueger@xamarin.com>
2013-08-08 07:02:19 +0200

    Added missing file.

---
+++ ICSharpCode.NRefactory.CSharp/Refactoring/CodeIssues/Synced/ConstraintViolations/InconsistentNamingIssue/UnderscoreHandling.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ICSharpCode.NRefactory.CSharp
+{
+	public enum UnderscoreHandling
+	{
+		Forbid,
+		Allow,
+		AllowWithLowerStartingLetter,
+		AllowWithUpperStartingLetter
+	}
+}
+
