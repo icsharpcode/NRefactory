a6b8ca7269674d145ff7d561c99430b91bb7d0b3
Mike Krüger <mkrueger@xamarin.com>
2013-08-16 10:56:04 +0200

    Added 'UseMethodIsInstanceOfTypeIssue'.

--- ICSharpCode.NRefactory.Tests/CSharp/CodeIssues/UseMethodIsInstanceOfTypeIssueTests.cs
+++ ICSharpCode.NRefactory.Tests/CSharp/CodeIssues/UseMethodIsInstanceOfTypeIssueTests.cs
@@ -1,21 +1,21 @@
-﻿// 
-// UnusedTypeParameterIssueTests.cs
-// 
+//
+// UseMethodIsInstanceOfTypeIssueTests.cs
+//
 // Author:
-//      Mansheng Yang <lightyang0@gmail.com>
-// 
-// Copyright (c) 2012 Mansheng Yang <lightyang0@gmail.com>
-// 
+//       Mike Krüger <mkrueger@xamarin.com>
+//
+// Copyright (c) 2013 Xamarin Inc. (http://xamarin.com)
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,46 +23,57 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
 using ICSharpCode.NRefactory.CSharp.Refactoring;
 using NUnit.Framework;
 
 namespace ICSharpCode.NRefactory.CSharp.CodeIssues
 {
 	[TestFixture]
-	public class UnusedTypeParameterIssueTests : InspectionActionTestBase
+	public class UseMethodIsInstanceOfTypeIssueTests : InspectionActionTestBase
 	{
-
 		[Test]
-		public void TestUnusedTypeParameter ()
+		public void TestBasicUsage ()
 		{
-			var input = @"
-class TestClass {
-	void TestMethod<T> ()
+			Test<UseMethodIsInstanceOfTypeIssue> (@"using System;
+class FooBar
+{
+	void Foo()
 	{
+		Type t;
+		if (t.IsAssignableFrom (this.GetType ())) {
+		}
 	}
-}";
-			Test<UnusedTypeParameterIssue> (input, 1);
+}
+", @"using System;
+class FooBar
+{
+	void Foo()
+	{
+		Type t;
+		if (t.IsInstanceOfType (this)) {
+		}
+	}
+}
+");
 		}
 
 		[Test]
-		public void TestUsedTypeParameter ()
+		public void TestDisable ()
 		{
-			var input = @"
-class TestClass {
-	void TestMethod<T> (T i)
+			TestWrongContext<UseMethodIsInstanceOfTypeIssue> (@"using System;
+class FooBar
+{
+	void Foo()
 	{
+		Type t;
+		// ReSharper disable once UseMethodIsInstanceOfType
+		if (t.IsAssignableFrom (this.GetType ())) {
+		}
 	}
-}";
-			var input2 = @"
-class TestClass {
-	T TestMethod<T> ()
-	{
-	}
-}";
-			Test<UnusedTypeParameterIssue> (input, 0);
-			Test<UnusedTypeParameterIssue> (input2, 0);
+}
+");
 		}
 
 	}
 }
+
