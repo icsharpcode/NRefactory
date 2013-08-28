f664c876e302e7ba690abe982f87d3dff16d9752
Mike Krüger <mkrueger@xamarin.com>
2013-08-28 14:34:58 +0200

    Improved some code actions.

--- ICSharpCode.NRefactory.CSharp.Refactoring/CodeActions/NegateRelationalExpressionAction.cs
+++ ICSharpCode.NRefactory.CSharp.Refactoring/CodeActions/NegateRelationalExpressionAction.cs
@@ -1,21 +1,23 @@
-﻿// 
+﻿//
 // NegateRelationalExpressionAction.cs
-// 
+//
 // Author:
 //      Mansheng Yang <lightyang0@gmail.com>
-// 
+//      Mike Krüger <mkrueger@xamarin.com>
+//
+// Copyright (c) 2013 Xamarin Inc. (http://xamarin.com)
 // Copyright (c) 2012 Mansheng Yang <lightyang0@gmail.com>
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,21 +26,45 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+
 namespace ICSharpCode.NRefactory.CSharp.Refactoring
 {
 	[ContextAction ("Negate a relational expression", Description = "Negate a relational expression.")]
-	public class NegateRelationalExpressionAction : SpecializedCodeAction<BinaryOperatorExpression>
+	public class NegateRelationalExpressionAction : CodeActionProvider
 	{
-		protected override CodeAction GetAction (RefactoringContext context, BinaryOperatorExpression node)
+		public static bool GetLogicalExpression (RefactoringContext context, out Expression expr, out AstNode token)
 		{
-			var newOp = CSharpUtil.NegateRelationalOperator (node.Operator);
-			if (newOp != BinaryOperatorType.Any && node.OperatorToken.Contains (context.Location)) {
-				return new CodeAction (string.Format (context.TranslateString ("Negate '{0}'"), node),
-					script => {
-						script.Replace (node, CSharpUtil.InvertCondition(node));
-					}, node.OperatorToken);
+			expr = null;
+			token = null;
+			var bOp = context.GetNode<BinaryOperatorExpression>();
+			if (bOp != null && bOp.OperatorToken.Contains(context.Location) && CSharpUtil.IsRelationalOperator (bOp.Operator)) {
+				expr = bOp;
+				token = bOp.OperatorToken;
+				return true;
 			}
-			return null;
+
+			var uOp = context.GetNode<UnaryOperatorExpression>();
+			if (uOp != null && uOp.OperatorToken.Contains(context.Location) && uOp.Operator == UnaryOperatorType.Not) {
+				expr = uOp;
+				token = uOp.OperatorToken;
+				return true;
+			}
+			return false;
+		}
+
+		public override System.Collections.Generic.IEnumerable<CodeAction> GetActions(RefactoringContext context)
+		{
+			Expression expr = null;
+			AstNode token;
+			if (!GetLogicalExpression (context, out expr, out token))
+				yield break;
+			yield return new CodeAction (
+				string.Format (context.TranslateString ("Negate '{0}'"), expr),
+				script => {
+					script.Replace (expr, CSharpUtil.InvertCondition(expr));
+				}, 
+				token
+			);
 		}
 	}
 }
