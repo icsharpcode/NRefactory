3ddebb399b0f04714ff57c56cb1b186fb330aadc
Mike Krüger <mkrueger@xamarin.com>
2013-10-18 05:38:37 +0200

    Enhanced 'GenerateSwitchLabelsAction'.

--- ICSharpCode.NRefactory.CSharp.Refactoring/CodeActions/GenerateSwitchLabelsAction.cs
+++ ICSharpCode.NRefactory.CSharp.Refactoring/CodeActions/GenerateSwitchLabelsAction.cs
@@ -27,6 +27,8 @@ using System;
 using ICSharpCode.NRefactory.TypeSystem;
 using System.Threading;
 using System.Collections.Generic;
+using ICSharpCode.NRefactory.Semantics;
+using System.Linq;
 
 namespace ICSharpCode.NRefactory.CSharp.Refactoring
 {
@@ -35,52 +37,87 @@ namespace ICSharpCode.NRefactory.CSharp.Refactoring
 	{
 		public override IEnumerable<CodeAction> GetActions(RefactoringContext context)
 		{
-			var switchStatement = GetSwitchStatement(context);
-			if (switchStatement == null) {
+			var switchStatement = context.GetNode<SwitchStatement> ();
+
+			if (switchStatement == null || !switchStatement.SwitchToken.Contains(context.Location))
 				yield break;
-			}
 			var result = context.Resolve(switchStatement.Expression);
-			if (result.Type.Kind != TypeKind.Enum) {
+			if (result.Type.Kind != TypeKind.Enum)
 				yield break;
-			}
-			yield return new CodeAction (context.TranslateString("Create switch labels"), script => {
-				var type = result.Type;
-				var newSwitch = (SwitchStatement)switchStatement.Clone();
-				
-				var target = context.CreateShortType(result.Type);
-				foreach (var field in type.GetFields ()) {
-					if (field.IsSynthetic || !field.IsConst) {
-						continue;
+
+			if (switchStatement.SwitchSections.Count == 0) {
+				yield return new CodeAction(context.TranslateString("Create switch labels"), script => {
+					var type = result.Type;
+					var newSwitch = (SwitchStatement)switchStatement.Clone();
+					
+					var target = context.CreateShortType(result.Type);
+					foreach (var field in type.GetFields ()) {
+						if (field.IsSynthetic || !field.IsConst)
+							continue;
+						newSwitch.SwitchSections.Add(new SwitchSection() {
+							CaseLabels = {
+								new CaseLabel(new MemberReferenceExpression(target.Clone(), field.Name))
+							},
+							Statements = {
+								new BreakStatement()
+							}
+						});
 					}
-					newSwitch.SwitchSections.Add(new SwitchSection () {
+					
+					newSwitch.SwitchSections.Add(new SwitchSection() {
 						CaseLabels = {
-							new CaseLabel (new MemberReferenceExpression (target.Clone(), field.Name))
+							new CaseLabel()
 						},
 						Statements = {
-							new BreakStatement ()
+							new ThrowStatement(new ObjectCreateExpression(context.CreateShortType("System", "ArgumentOutOfRangeException")))
 						}
 					});
-				}
-				
-				newSwitch.SwitchSections.Add(new SwitchSection () {
-					CaseLabels = {
-						new CaseLabel ()
-					},
-					Statements = {
-						new ThrowStatement (new ObjectCreateExpression (context.CreateShortType("System", "ArgumentOutOfRangeException")))
+					
+					script.Replace(switchStatement, newSwitch);
+				}, switchStatement);
+			} else {
+				var missingFields = new List<IField>();
+				foreach (var field in result.Type.GetFields ()) {
+					if (field.IsSynthetic || !field.IsConst)
+						continue;
+					if (!IsHandled(context, switchStatement, field)) {
+						missingFields.Add(field); 
 					}
-				});
-				
-				script.Replace(switchStatement, newSwitch);
-			}, switchStatement);
+				}
+				if (missingFields.Count == 0)
+					yield break;
+				yield return new CodeAction(context.TranslateString("Create missing switch labels"), script => {
+					var type = result.Type;
+					//var newSwitch = (SwitchStatement)switchStatement.Clone();
+					var insertNode = (AstNode)switchStatement.SwitchSections.LastOrDefault(s => !s.CaseLabels.Any(label => label.Expression.IsNull)) ?? switchStatement.LBraceToken;
+
+					var target = context.CreateShortType(result.Type);
+					foreach (var field in missingFields) {
+						script.InsertAfter(insertNode, new SwitchSection() {
+							CaseLabels =  {
+								new CaseLabel(new MemberReferenceExpression(target.Clone(), field.Name))
+							},
+							Statements =  {
+								new BreakStatement()
+							}
+						});
+					}
+				}, switchStatement);
+			}
 		}
-		
-		static SwitchStatement GetSwitchStatement (RefactoringContext context)
+
+		static bool IsHandled(RefactoringContext context, SwitchStatement switchStatement, IField field)
 		{
-			var switchStatment = context.GetNode<SwitchStatement> ();
-			if (switchStatment != null && switchStatment.SwitchSections.Count == 0)
-				return switchStatment;
-			return null;
+			foreach (var sect in switchStatement.SwitchSections) {
+				foreach (var caseLabel in sect.CaseLabels) {
+					var resolveCase = context.Resolve(caseLabel.Expression) as MemberResolveResult;
+					if (resolveCase == null)
+						continue;
+					if (field == resolveCase.Member)
+						return true;
+				}
+			}
+			return false;
 		}
 	}
 }
