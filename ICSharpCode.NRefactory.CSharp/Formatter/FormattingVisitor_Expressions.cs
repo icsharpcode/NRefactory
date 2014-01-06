e4f8b23e1723669dbe2ccffa6813d6917480c71f
Mike Krüger <mkrueger@xamarin.com>
2014-01-06 12:53:41 +0100

    Fixed formatting bug.

--- ICSharpCode.NRefactory.CSharp/Formatter/FormattingVisitor_Expressions.cs
+++ ICSharpCode.NRefactory.CSharp/Formatter/FormattingVisitor_Expressions.cs
@@ -451,14 +451,14 @@ namespace ICSharpCode.NRefactory.CSharp
 				ForceSpacesAfter(invocationExpression.LParToken, policy.SpaceBetweenEmptyMethodCallParentheses);
 				ForceSpacesBefore(invocationExpression.RParToken, policy.SpaceBetweenEmptyMethodCallParentheses);
 			}
-
+			bool popIndent = false;
 			if (invocationExpression.Target is MemberReferenceExpression) {
 				var mt = (MemberReferenceExpression)invocationExpression.Target;
 				if (mt.Target is InvocationExpression) {
 					if (DoWrap(policy.ChainedMethodCallWrapping, mt.DotToken, 2)) {
-						curIndent.Push(IndentType.Continuation);
+						curIndent.Push(IndentType.Block);
+						popIndent = true;
 						FixStatementIndentation(mt.DotToken.StartLocation);
-						curIndent.Pop();
 					} else {
 						if (policy.ChainedMethodCallWrapping == Wrapping.DoNotWrap)
 							ForceSpacesBeforeRemoveNewLines(mt.DotToken, false);
@@ -466,6 +466,8 @@ namespace ICSharpCode.NRefactory.CSharp
 				}
 			}
 			FormatArguments(invocationExpression);
+			if (popIndent)
+				curIndent.Pop();
 		}
 
 		public override void VisitIndexerExpression(IndexerExpression indexerExpression)
