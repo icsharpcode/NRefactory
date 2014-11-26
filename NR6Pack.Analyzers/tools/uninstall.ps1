0197423fda66df8e480a62b7f71ed55b631e37e2
Andreas Weizel <rpinski@awzhome.de>
2014-11-26 01:54:17 +0100

    Added first skeleton of NR6Pack.Analyzers project.

---
+++ NR6Pack.Analyzers/tools/uninstall.ps1
@@ -0,0 +1,6 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$analyzerPath = join-path $toolsPath "analyzers"
+$analyzerFilePath = join-path $analyzerPath "NR6Pack.Analyzers.dll"
+
+$project.Object.AnalyzerReferences.Remove("$analyzerFilePath")
