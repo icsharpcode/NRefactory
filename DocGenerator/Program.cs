05b4a548da7fd53a21161f4673b2871dde3aff70
Siegfried Pammer <siegfriedpammer@gmail.com>
2014-11-28 19:53:48 +0100

    Add DocGenerator to generate the list of included CodeActions

---
+++ DocGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+
+namespace DocGenerator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var types = typeof(ICSharpCode.NRefactory6.CSharp.DescriptionAttribute).Assembly.GetTypes()
+				.Where(t => t.CustomAttributes.Any(a => a.AttributeType.FullName == typeof(ExportCodeRefactoringProviderAttribute).FullName));
+
+			foreach (var t in types) {
+				Console.WriteLine("<li>{0} ({1})</li>", HttpUtility.HtmlEncode(GetDescription(t)), t.Name);
+			}
+
+			Console.ReadKey();
+		}
+
+		private static string GetDescription(Type t)
+		{
+			var description = t.GetCustomAttributes(false).OfType<ICSharpCode.NRefactory6.CSharp.DescriptionAttribute>().FirstOrDefault();
+			if (description != null && description.Description.Length > 0)
+				return description.Description;
+			var exportAttribute = t.GetCustomAttributes(false).OfType<ExportCodeRefactoringProviderAttribute>().First();
+			return exportAttribute.Name;
+		}
+	}
+}
