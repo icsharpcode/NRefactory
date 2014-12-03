3e058650345ce06434c95dad7228f0132251a76f
Andreas Weizel <rpinski@awzhome.de>
2014-12-03 23:59:39 +0100

    Introduced auto-versioning of NR6Pack.Vsix for debug builds through pre-build action.

---
+++ NR6Pack.Vsix/ModifyVsixVersion.ps1
@@ -0,0 +1,20 @@
+﻿# Automatically update the version in VSIX manifest
+if ($args.Length -lt 2)
+{
+	exit
+}
+
+$debugMode = ($args[0] -eq "Debug")
+$workingDir = $args[1]
+$templateFile = (join-path $workingDir "source.extension.template.vsixmanifest")
+$targetFile = (join-path $workingDir "source.extension.vsixmanifest")
+
+$version = 0
+if ($debugMode)
+{
+	# Replace version placeholder with random generated version
+	$random = new-object System.Random
+	$version = $random.Next(65534) + 1;
+}
+
+Get-Content $templateFile | ForEach-Object { $_.Replace('%%version%%', $version) } | Set-Content $targetFile
