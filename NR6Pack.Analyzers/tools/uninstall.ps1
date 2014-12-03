48b0735ab772df519492a9119bd78da082c76d5a
Andreas Weizel <rpinski@awzhome.de>
2014-12-03 20:54:10 +0100

    Removed NR6Pack.Analyzers project.

--- NR6Pack.Analyzers/tools/uninstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$analyzerPath = join-path $toolsPath "analyzers"
-$analyzerFilePath = join-path $analyzerPath "NR6Pack.Analyzers.dll"
-
-$project.Object.AnalyzerReferences.Remove("$analyzerFilePath")
