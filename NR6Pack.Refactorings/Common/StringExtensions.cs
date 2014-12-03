806a94d62abb866441c379bbf46012a5ba584320
Andreas Weizel <rpinski@awzhome.de>
2014-12-03 20:20:23 +0100

    Merged NR6Pack.Analyzers project back to NR6Pack.Refactorings, now containing CodeActions as well as CodeIssues again.

---
+++ NR6Pack.Refactorings/Common/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICSharpCode.NRefactory6.CSharp
+{
+	static class StringExtensions
+	{
+		public static bool Any(this string str, Func<char, bool> predicate)
+		{
+			for (int i = 0; i < str.Length; i++)
+			{
+				if (predicate(str[i]))
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
