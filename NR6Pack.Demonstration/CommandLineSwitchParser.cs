d2640256816c14d98382e6afe27d0ba08c6a4590
Andreas Weizel <rpinski@awzhome.de>
2014-12-11 01:42:13 +0100

    Added code showing some working CodeAction/CodeIssues to demonstration project.

---
+++ NR6Pack.Demonstration/CommandLineSwitchParser.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR6Pack.Demonstration
+{
+	public enum CommandLineSwitches
+	{
+		A,
+		B,
+		C,
+		D,
+		V
+	}
+
+	public class CommandLineSwitchParser
+	{
+		// Bulb: private is optional -> suggestion to remove it
+		private CommandLineSwitches switches;
+		private string[] parameters;
+
+		// Info: Empty default constructor is optional and can be removed
+		public CommandLineSwitchParser()
+		{
+
+		}
+
+		// As soon as this constructor is activated, the default constructor above is no more marked as optional
+		//public CommandLineSwitchParser(string[] parameters)
+		//{
+		//	this.parameters = parameters;
+		//}
+
+		// Info: Empty destructor can be removed, too.
+		~CommandLineSwitchParser()
+		{
+
+		}
+
+		public string[] Parameters
+		{
+			get
+			{
+				return parameters;
+			}
+
+			set
+			{
+				parameters = value;
+			}
+		}
+
+		// CodeAction: Add another accessor (means 'set { ... }' here)
+		public CommandLineSwitches Switches
+		{
+            get
+			{
+				return switches;
+			}
+		}
+
+		// CodeAction: Method can be converted to non-virtual
+		public virtual void Parse()
+		{
+			// CodeAction: Remove braces from 'if' and vice versa
+			if (Parameters == null)
+			{
+				return;
+			}
+
+			// CodeAction: Use 'var' instead of 'string' on param
+			// CodeAction: Conversion of foreach to for loop
+			switches = 0;
+			foreach (string param in Parameters)
+			{
+				switch (param)
+				{
+					case "-a":
+						// CodeIssue should warn here about bit operation on enum, which is not annotated with [Flags]
+						switches |= CommandLineSwitches.A;
+						break;
+					case "-b":
+						switches |= CommandLineSwitches.B;
+						break;
+					case "-c":
+						switches |= CommandLineSwitches.C;
+						break;
+					case "-d":
+						switches |= CommandLineSwitches.D;
+						break;
+					case "-v":
+						switches |= CommandLineSwitches.V;
+						break;
+				}
+			}
+		}
+	}
+}
--- NR6Pack.Demonstration/Program.cs
+++ NR6Pack.Demonstration/Program.cs
@@ -10,6 +10,46 @@ namespace NR6Pack.Demonstration
 	{
 		static void Main(string[] args)
 		{
+			// CompareFloats() shows some detected comparison issues with floats
+			CompareFloats();
+
+			// Compare exactly the same constant value -> suggestion to convert to 'true'
+			// Also other CodeActions: Convert to Equals() call, invert and negate condition etc.
+			bool fiveIsFive = (5 == 5);
+
+			// CodeAction: Use explicit type instead of 'var'
+			var commandLineSwitchParser = new CommandLineSwitchParser();
+			commandLineSwitchParser.Parameters = args;
+			commandLineSwitchParser.Parse();
+
+			Console.WriteLine("Selected switches: {0}", commandLineSwitchParser.Switches);
+		}
+
+		static void CompareFloats()
+		{
+			// Doing some float comparison will be marked as potential issue
+			float float1 = 1.0f;
+			float float2 = 1.0f;
+
+			if (float1 == float2)
+			{
+				// Default equality test might fail on floats
+			}
+
+			if (float1 != 0)
+			{
+				// Same for zero comparison
+			}
+
+			if (float1 == float.NaN)
+			{
+				// Not a good way to test for NaN
+			}
+
+			if (float1 != float.PositiveInfinity)
+			{
+				// Not a good way to test for infinity
+			}
 		}
 	}
 }
