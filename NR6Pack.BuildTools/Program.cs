f39ea24adc84ef156b03fc48b536e2d12e2a236f
Siegfried Pammer <siegfriedpammer@gmail.com>
2015-01-27 07:53:29 +0100

    add BuildTools project to get rid of powershell dependency in VSIX build

---
+++ NR6Pack.BuildTools/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NR6Pack.BuildTools
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool isDebug = (args.FirstOrDefault() ?? "") == "Debug";
+			string basePath = @"..\NR6Pack.Vsix\";
+			string templateFile = basePath + "source.extension.template.vsixmanifest";
+			string targetFile = basePath + "source.extension.vsixmanifest";
+			int version = isDebug ? new Random().Next(65534) + 1 : 0;
+			File.WriteAllText(targetFile, File.ReadAllText(templateFile).Replace("%%version%%", version.ToString()));
+		}
+	}
+}
