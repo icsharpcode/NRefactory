05b3776abc09a8dbdc113596a05256a689943bcb
Mike Krüger <mkrueger@xamarin.com>
2015-03-27 13:27:53 +0100

    Added 'CastExpressionSyntaxExtensions'.

---
+++ ICSharpCode.NRefactory.CSharp/Util/CastExpressionSyntaxExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis;
+using System;
+using System.Reflection;
+
+namespace ICSharpCode.NRefactory6.CSharp
+{
+	public static class CastExpressionSyntaxExtensions
+	{
+		readonly static MethodInfo isUnnecessaryCastMethod;
+
+		static CastExpressionSyntaxExtensions ()
+		{
+			var typeInfo = Type.GetType ("Microsoft.CodeAnalysis.CSharp.Extensions.CastExpressionSyntaxExtensions" + ReflectionNamespaces.CSWorkspacesAsmName, true);
+			isUnnecessaryCastMethod = typeInfo.GetMethod ("IsUnnecessaryCast", BindingFlags.Static | BindingFlags.Public);
+		}
+
+		public static bool IsUnnecessaryCast(this CastExpressionSyntax cast, SemanticModel semanticModel, CancellationToken cancellationToken)
+		{
+			return (bool)isUnnecessaryCastMethod.Invoke (null, new object[] { cast, semanticModel, cancellationToken });
+		}
+	}
+}
