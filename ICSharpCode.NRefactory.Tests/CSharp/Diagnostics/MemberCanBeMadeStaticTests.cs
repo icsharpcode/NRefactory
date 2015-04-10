f822d4f8eab5b0e8bcdbd3c3e44ea11e0009d13e
Mike Krüger <mkrueger@xamarin.com>
2015-04-10 20:40:31 +0200

    Fixed build.

--- ICSharpCode.NRefactory.Tests/CSharp/Diagnostics/MemberCanBeMadeStaticTests.cs
+++ ICSharpCode.NRefactory.Tests/CSharp/Diagnostics/MemberCanBeMadeStaticTests.cs
@@ -32,310 +32,310 @@ namespace ICSharpCode.NRefactory6.CSharp.Diagnostics
 	[Ignore("TODO: Issue not ported yet")]
 	public class MemberCanBeMadeStaticTests : InspectionActionTestBase
 	{
-		[Test]
-		public void TestPrivateMethod()
-		{
-			TestWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
-				@"class TestClass
-{
-	void $Test ()
-	{
-		int a = 2;
-	}
-}",
-				@"class TestClass
-{
-	static void Test ()
-	{
-		int a = 2;
-	}
-}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate
-			);
-		}
+//		[Test]
+//		public void TestPrivateMethod()
+//		{
+//			TestWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
+//				@"class TestClass
+//{
+//	void $Test ()
+//	{
+//		int a = 2;
+//	}
+//}",
+//				@"class TestClass
+//{
+//	static void Test ()
+//	{
+//		int a = 2;
+//	}
+//}"
+//			);
+//		}
 
-		[Test]
-		public void TestPrivateMethodPublicSkip()
-		{
-			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
-				@"class TestClass
-{
-	void $Test ()
-	{
-		int a = 2;
-	}
-}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate
-			);
-		}
+//		[Test]
+//		public void TestPrivateMethodPublicSkip()
+//		{
+//			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
+//				@"class TestClass
+//{
+//	void $Test ()
+//	{
+//		int a = 2;
+//	}
+//}"
+//			);
+//		}
 
-		[Test]
-		public void TestPublicMethod()
-		{
-			TestWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
-				@"class TestClass
-{
-	public void $Test ()
-	{
-		int a = 2;
-	}
-}",
-				@"class TestClass
-{
-	public static void Test ()
-	{
-		int a = 2;
-	}
-}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate
-			);
-		}
+//		[Test]
+//		public void TestPublicMethod()
+//		{
+//			TestWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
+//				@"class TestClass
+//{
+//	public void $Test ()
+//	{
+//		int a = 2;
+//	}
+//}",
+//				@"class TestClass
+//{
+//	public static void Test ()
+//	{
+//		int a = 2;
+//	}
+//}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate
+//			);
+//		}
 
-		[Test]
-		public void TestPublicMethodSkip()
-		{
-			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
-				@"class TestClass
-{
-	public void $Test ()
-	{
-		int a = 2;
-	}
-}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate
-			);
-		}
+//		[Test]
+//		public void TestPublicMethodSkip()
+//		{
+//			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
+//				@"class TestClass
+//{
+//	public void $Test ()
+//	{
+//		int a = 2;
+//	}
+//}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate
+//			);
+//		}
 
-		[Test]
-		public void MethodThatCallsInstanceMethodOnParameter()
-		{
-			TestWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
-				@"class TestClass
-{
-	string $Test (string txt)
-	{
-		return txt.Trim ();
-	}
-}",
-				@"class TestClass
-{
-	static string Test (string txt)
-	{
-		return txt.Trim ();
-	}
-}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate
-			);
-		}
+//		[Test]
+//		public void MethodThatCallsInstanceMethodOnParameter()
+//		{
+//			TestWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
+//				@"class TestClass
+//{
+//	string $Test (string txt)
+//	{
+//		return txt.Trim ();
+//	}
+//}",
+//				@"class TestClass
+//{
+//	static string Test (string txt)
+//	{
+//		return txt.Trim ();
+//	}
+//}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate
+//			);
+//		}
 
-		[Test]
-		public void TestWithVirtualFunction()
-		{
+//		[Test]
+//		public void TestWithVirtualFunction()
+//		{
 			
-			var input = @"class TestClass
-{
-	public virtual void $Test ()
-	{
-		int a = 2;
-	}
-}";
-			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate);
-		}
+//			var input = @"class TestClass
+//{
+//	public virtual void $Test ()
+//	{
+//		int a = 2;
+//	}
+//}";
+//			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate);
+//		}
 
-		[Test]
-		public void TestWithInterfaceImplementation()
-		{
-			var input = @"interface IBase {
-    void Test();
-}
-class TestClass : IBase
-{
-	public void $Test ()
-	{
-		int a = 2;
-	}
-}";
-			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate);
-		}
+//		[Test]
+//		public void TestWithInterfaceImplementation()
+//		{
+//			var input = @"interface IBase {
+//    void Test();
+//}
+//class TestClass : IBase
+//{
+//	public void $Test ()
+//	{
+//		int a = 2;
+//	}
+//}";
+//			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate);
+//		}
 
-		[Test]
-		public void TestWithStaticFunction()
-		{
+//		[Test]
+//		public void TestWithStaticFunction()
+//		{
 
-			var input = @"class TestClass
-{
-	static void $Test ()
-	{
-		int a = 2;
-	}
-}";
-			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate);
-		}
+//			var input = @"class TestClass
+//{
+//	static void $Test ()
+//	{
+//		int a = 2;
+//	}
+//}";
+//			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate);
+//		}
 
-		[Test]
-		public void TestDoNotWarnOnAttributes()
-		{
+//		[Test]
+//		public void TestDoNotWarnOnAttributes()
+//		{
 
-			var input = @"using System;
-class TestClass
-{
-	[Obsolete]
-	public void $Test ()
-	{
-		int a = 2;
-	}
-}";
-			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate);
-		}
+//			var input = @"using System;
+//class TestClass
+//{
+//	[Obsolete]
+//	public void $Test ()
+//	{
+//		int a = 2;
+//	}
+//}";
+//			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate);
+//		}
 
-		[Test]
-		public void TestDoNotWarnOnEmptyMethod()
-		{
+//		[Test]
+//		public void TestDoNotWarnOnEmptyMethod()
+//		{
 
-			var input = @"using System;
-class TestClass
-{
-	public void $Test ()
-	{
-	}
-}";
-			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate);
-		}
+//			var input = @"using System;
+//class TestClass
+//{
+//	public void $Test ()
+//	{
+//	}
+//}";
+//			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate);
+//		}
 
-		[Test]
-		public void TestDoNotWarnOnInterfaceMethod()
-		{
+//		[Test]
+//		public void TestDoNotWarnOnInterfaceMethod()
+//		{
 
-			var input = @"using System;
-interface ITestInterface
-{
-	void $Test ();
-}";
-			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate);
-		}
+//			var input = @"using System;
+//interface ITestInterface
+//{
+//	void $Test ();
+//}";
+//			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate);
+//		}
 
-		[Test]
-		public void TestDoNotWarnOnNotImplementedMethod()
-		{
-			var input = @"using System;
-class TestClass
-{
-	public void $Test ()
-	{
-		throw new NotImplementedExceptionIssue();
-	}
-}";
-			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate);
-		}
+//		[Test]
+//		public void TestDoNotWarnOnNotImplementedMethod()
+//		{
+//			var input = @"using System;
+//class TestClass
+//{
+//	public void $Test ()
+//	{
+//		throw new NotImplementedExceptionIssue();
+//	}
+//}";
+//			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate);
+//		}
 
-		[Test]
-		public void TestPropertyAccess()
-		{
-			var input = @"using System;
-class TestClass
-{
-	public int Foo { get; set; }
-	public void $Test ()
-	{
-		System.Console.WriteLine (Foo);
-	}
-}";
-			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate);
-		}
+//		[Test]
+//		public void TestPropertyAccess()
+//		{
+//			var input = @"using System;
+//class TestClass
+//{
+//	public int Foo { get; set; }
+//	public void $Test ()
+//	{
+//		System.Console.WriteLine (Foo);
+//	}
+//}";
+//			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate);
+//		}
 
-		[Test]
-		public void DoNotWarnOnMarshalByRefObject()
-		{
+//		[Test]
+//		public void DoNotWarnOnMarshalByRefObject()
+//		{
 			
-			var input = @"class TestClass : System.MarshalByRefObject
-{
-	public void $Test ()
-	{
-		int a = 2;
-	}
-}";
-			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate);
-		}
+//			var input = @"class TestClass : System.MarshalByRefObject
+//{
+//	public void $Test ()
+//	{
+//		int a = 2;
+//	}
+//}";
+//			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(input, MemberCanBeMadeStaticAnalyzer.DiagnosticIdNonPrivate);
+//		}
 
-		[Test]
-		public void TestProperty()
-		{
-			TestWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
-				@"class TestClass
-{
-	int Test {
-		get {
-			return 2;
-		}
-	}
-}",
-				@"class TestClass
-{
-	static int Test {
-		get {
-			return 2;
-		}
-	}
-}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate
-			);
-		}
+//		[Test]
+//		public void TestProperty()
+//		{
+//			TestWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
+//				@"class TestClass
+//{
+//	int Test {
+//		get {
+//			return 2;
+//		}
+//	}
+//}",
+//				@"class TestClass
+//{
+//	static int Test {
+//		get {
+//			return 2;
+//		}
+//	}
+//}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate
+//			);
+//		}
 
 
-		[Test]
-		public void TestCustomEvent()
-		{
-			TestWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
-				@"using System;
+//		[Test]
+//		public void TestCustomEvent()
+//		{
+//			TestWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
+//				@"using System;
 
-class TestClass
-{
-	static event EventHandler Foo;
+//class TestClass
+//{
+//	static event EventHandler Foo;
 
-	event EventHandler Bar {
-		add { Foo += value; }
-		remove { Foo -= value; }
-	}
-}",
-				@"using System;
+//	event EventHandler Bar {
+//		add { Foo += value; }
+//		remove { Foo -= value; }
+//	}
+//}",
+//				@"using System;
 
-class TestClass
-{
-	static event EventHandler Foo;
+//class TestClass
+//{
+//	static event EventHandler Foo;
 
-	static event EventHandler Bar {
-		add { Foo += value; }
-		remove { Foo -= value; }
-	}
-}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate
-				);
-		}
+//	static event EventHandler Bar {
+//		add { Foo += value; }
+//		remove { Foo -= value; }
+//	}
+//}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate
+//				);
+//		}
 
 		
-		[Test]
-		public void TestCustomEventOnNotImplemented()
-		{
-			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
-				@"using System;
+//		[Test]
+//		public void TestCustomEventOnNotImplemented()
+//		{
+//			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
+//				@"using System;
 
-class TestClass
-{
-	static event EventHandler Foo;
+//class TestClass
+//{
+//	static event EventHandler Foo;
 
-	event EventHandler Bar {
-		add { throw new NotImplementedException (); }
-		remove { throw new NotImplementedException (); }
-	}
-}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate
-				);
-		}
+//	event EventHandler Bar {
+//		add { throw new NotImplementedException (); }
+//		remove { throw new NotImplementedException (); }
+//	}
+//}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate
+//				);
+//		}
 
-		[Test]
-		public void TestDisable()
-		{
-			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
-				@"class TestClass
-{
-// ReSharper disable once MemberCanBeMadeStatic.Local
-	void Test ()
-	{
-		int a = 2;
-	}
-}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate);
-		}
+//		[Test]
+//		public void TestDisable()
+//		{
+//			TestWrongContextWithSubIssue<MemberCanBeMadeStaticAnalyzer>(
+//				@"class TestClass
+//{
+//// ReSharper disable once MemberCanBeMadeStatic.Local
+//	void Test ()
+//	{
+//		int a = 2;
+//	}
+//}", MemberCanBeMadeStaticAnalyzer.DiagnosticIdPrivate);
+//		}
 	}
 }
