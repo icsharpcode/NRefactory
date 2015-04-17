e6cfb8f8c109c08daccec9ed478a6e797d07261c
Mike Krüger <mkrueger@xamarin.com>
2015-04-17 10:21:46 +0200

    Fixed diagnostic handling for ConvertIfToAndExpressionCodeFixProvider.

--- ICSharpCode.NRefactory.CSharp.Refactoring/Diagnostics/Custom/ConvertIfToAndExpressionCodeFixProvider.cs
+++ ICSharpCode.NRefactory.CSharp.Refactoring/Diagnostics/Custom/ConvertIfToAndExpressionCodeFixProvider.cs
@@ -43,7 +43,6 @@ using Microsoft.CodeAnalysis.FindSymbols;
 
 namespace ICSharpCode.NRefactory6.CSharp.Diagnostics
 {
-
 	[ExportCodeFixProvider(LanguageNames.CSharp), System.Composition.Shared]
 	public class ConvertIfToAndExpressionCodeFixProvider : CodeFixProvider
 	{
@@ -63,52 +62,51 @@ namespace ICSharpCode.NRefactory6.CSharp.Diagnostics
 			var document = context.Document;
 			var cancellationToken = context.CancellationToken;
 			var span = context.Span;
-			var diagnostics = context.Diagnostics;
+			var diagnostic = context.Diagnostics.First ();
 			var root = await document.GetSyntaxRootAsync(cancellationToken);
-			foreach (var diagnostic in diagnostics)
+			var node = root.FindToken(context.Span.Start).Parent as IfStatementSyntax;
+			if (node == null)
+				return;
+			ExpressionSyntax target;
+			SyntaxTriviaList assignmentTrailingTriviaList;
+			ConvertIfToOrExpressionAnalyzer.MatchIfElseStatement(node, SyntaxKind.FalseLiteralExpression, out target, out assignmentTrailingTriviaList);
+			SyntaxNode newRoot = null;
+			var varDeclaration = ConvertIfToOrExpressionAnalyzer.FindPreviousVarDeclaration(node);
+			if (varDeclaration != null)
 			{
-				var node = root.FindNode(context.Span) as IfStatementSyntax;
-				ExpressionSyntax target;
-				SyntaxTriviaList assignmentTrailingTriviaList;
-				ConvertIfToOrExpressionAnalyzer.MatchIfElseStatement(node, SyntaxKind.FalseLiteralExpression, out target, out assignmentTrailingTriviaList);
-				SyntaxNode newRoot = null;
-				var varDeclaration = ConvertIfToOrExpressionAnalyzer.FindPreviousVarDeclaration(node);
-				if (varDeclaration != null)
+				var varDeclarator = varDeclaration.Declaration.Variables[0];
+				newRoot = root.ReplaceNodes(new SyntaxNode[] { varDeclaration, node }, (arg, arg2) =>
 				{
-					var varDeclarator = varDeclaration.Declaration.Variables[0];
-					newRoot = root.ReplaceNodes(new SyntaxNode[] { varDeclaration, node }, (arg, arg2) =>
-					{
-						if (arg is LocalDeclarationStatementSyntax)
-							return SyntaxFactory.LocalDeclarationStatement(
-									SyntaxFactory.VariableDeclaration(varDeclaration.Declaration.Type,
-										SyntaxFactory.SeparatedList(
-											new[] {
-												SyntaxFactory.VariableDeclarator(varDeclarator.Identifier.ValueText)
-													.WithInitializer(
-														SyntaxFactory.EqualsValueClause(
-															SyntaxFactory.BinaryExpression(SyntaxKind.LogicalAndExpression, ConvertIfToOrExpressionAnalyzer.AddParensToComplexExpression(varDeclarator.Initializer.Value), ConvertIfToOrExpressionAnalyzer.AddParensToComplexExpression(CSharpUtil.InvertCondition(node.Condition))))
-																.WithAdditionalAnnotations(Formatter.Annotation)
-													)
-											}
-										))
-								).WithLeadingTrivia(varDeclaration.GetLeadingTrivia()).WithTrailingTrivia(node.GetTrailingTrivia());
-						return null;
-					});
-				}
-				else
-				{
-                    newRoot = root.ReplaceNode((SyntaxNode)node,
-						SyntaxFactory.ExpressionStatement(
-							SyntaxFactory.AssignmentExpression(
-								SyntaxKind.AndAssignmentExpression,
-								ConvertIfToOrExpressionAnalyzer.AddParensToComplexExpression(target),
-								ConvertIfToOrExpressionAnalyzer.AddParensToComplexExpression(CSharpUtil.InvertCondition(node.Condition)).WithAdditionalAnnotations(Formatter.Annotation)
-							)
-						).WithLeadingTrivia(node.GetLeadingTrivia()).WithTrailingTrivia(node.GetTrailingTrivia()));
-				}
-
-				context.RegisterCodeFix(CodeActionFactory.Create(node.Span, diagnostic.Severity, diagnostic.GetMessage(), document.WithSyntaxRoot(newRoot)), diagnostic);
+					if (arg is LocalDeclarationStatementSyntax)
+						return SyntaxFactory.LocalDeclarationStatement(
+								SyntaxFactory.VariableDeclaration(varDeclaration.Declaration.Type,
+									SyntaxFactory.SeparatedList(
+										new[] {
+											SyntaxFactory.VariableDeclarator(varDeclarator.Identifier.ValueText)
+												.WithInitializer(
+													SyntaxFactory.EqualsValueClause(
+														SyntaxFactory.BinaryExpression(SyntaxKind.LogicalAndExpression, ConvertIfToOrExpressionAnalyzer.AddParensToComplexExpression(varDeclarator.Initializer.Value), ConvertIfToOrExpressionAnalyzer.AddParensToComplexExpression(CSharpUtil.InvertCondition(node.Condition))))
+															.WithAdditionalAnnotations(Formatter.Annotation)
+												)
+										}
+									))
+							).WithLeadingTrivia(varDeclaration.GetLeadingTrivia()).WithTrailingTrivia(node.GetTrailingTrivia());
+					return null;
+				});
 			}
+			else
+			{
+                newRoot = root.ReplaceNode((SyntaxNode)node,
+					SyntaxFactory.ExpressionStatement(
+						SyntaxFactory.AssignmentExpression(
+							SyntaxKind.AndAssignmentExpression,
+							ConvertIfToOrExpressionAnalyzer.AddParensToComplexExpression(target),
+							ConvertIfToOrExpressionAnalyzer.AddParensToComplexExpression(CSharpUtil.InvertCondition(node.Condition)).WithAdditionalAnnotations(Formatter.Annotation)
+						)
+					).WithLeadingTrivia(node.GetLeadingTrivia()).WithTrailingTrivia(node.GetTrailingTrivia()));
+			}
+
+			context.RegisterCodeFix(CodeActionFactory.Create(node.Span, diagnostic.Severity, diagnostic.GetMessage(), document.WithSyntaxRoot(newRoot)), diagnostic);
 		}
 	}
 }
