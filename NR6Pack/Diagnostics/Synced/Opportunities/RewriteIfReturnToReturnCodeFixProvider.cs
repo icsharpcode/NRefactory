2ef4490a2fccd542f9049d4a34d28cad0777c1f5
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-06-22 09:20:15 -0400

    merge commit for rewrite return

--- NR6Pack/Diagnostics/Synced/Opportunities/RewriteIfReturnToReturnCodeFixProvider.cs
+++ NR6Pack/Diagnostics/Synced/Opportunities/RewriteIfReturnToReturnCodeFixProvider.cs
@@ -44,33 +44,43 @@ using Microsoft.CodeAnalysis.FindSymbols;
 namespace ICSharpCode.NRefactory6.CSharp.Diagnostics
 {
 
-	[ExportCodeFixProvider(LanguageNames.CSharp), System.Composition.Shared]
-	public class RewriteIfReturnToReturnCodeFixProvider : CodeFixProvider
-	{
-		public override ImmutableArray<string> FixableDiagnosticIds {
-			get {
-				return ImmutableArray.Create (NRefactoryDiagnosticIDs.RewriteIfReturnToReturnAnalyzerID);
-			}
-		}
+    [ExportCodeFixProvider(LanguageNames.CSharp), System.Composition.Shared]
+    public class RewriteIfReturnToReturnCodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(NRefactoryDiagnosticIDs.RewriteIfReturnToReturnAnalyzerID); }
+        }
 
-		public override FixAllProvider GetFixAllProvider()
-		{
-			return WellKnownFixAllProviders.BatchFixer;
-		}
+        public override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
 
-		public async override Task RegisterCodeFixesAsync(CodeFixContext context)
-		{
-			var document = context.Document;
-			var cancellationToken = context.CancellationToken;
-			var span = context.Span;
-			var diagnostics = context.Diagnostics;
-			var root = await document.GetSyntaxRootAsync(cancellationToken);
-			var diagnostic = diagnostics.First ();
-			var node = root.FindNode(context.Span);
-			//if (!node.IsKind(SyntaxKind.BaseList))
-			//	continue;
-			var newRoot = root.RemoveNode(node, SyntaxRemoveOptions.KeepNoTrivia);
-			context.RegisterCodeFix(CodeActionFactory.Create(node.Span, diagnostic.Severity, "Convert to 'return' statement", document.WithSyntaxRoot(newRoot)), diagnostic);
-		}
-	}
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var document = context.Document;
+            var cancellationToken = context.CancellationToken;
+            var span = context.Span;
+            var diagnostics = context.Diagnostics;
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var diagnostic = diagnostics.First();
+            var node = root.FindNode(context.Span);
+            if (node == null)
+                return;
+
+            context.RegisterCodeFix(
+                CodeActionFactory.Create(node.Span, diagnostic.Severity, "Convert to 'return' statement", token =>
+                {
+                    var returnStatement = node.ChildNodes().OfType<ReturnStatementSyntax>().First();
+                    var newNode = SyntaxFactory.ReturnStatement(returnStatement.ReturnKeyword,
+                        (node as IfStatementSyntax).Condition, returnStatement.SemicolonToken);
+                    var newRoot = root.ReplaceNode((SyntaxNode)node,
+                        newNode.WithLeadingTrivia(node.GetLeadingTrivia())
+                            .WithAdditionalAnnotations(Formatter.Annotation));
+                    return Task.FromResult(document.WithSyntaxRoot(newRoot));
+                }), diagnostic);
+
+        }
+    }
 }
